1bf28bede91c24e421a8f8116aaa3dde34774155
kadepeay <kadepeay@gmail.com>
2020-05-07 00:53:00 -0600

    goof

--- src/main.py
+++ src/main.py
@@ -5,17 +5,18 @@ t = turtle.Turtle()
 wn = turtle.Screen()
 
 def draw():
-    colors = ['blue', 'purple', 'red', 'yellow', 'orange', 'green']
+    colors = ['blue', 'purple', 'red', 'yellow', 'orange', 'green',]
 
     t.pensize(5)
     wn.bgcolor('black')
     t.speed(1000)
 
-    for x in range(360):
+    for x in range(700):
         t.pencolor(colors[x % len(colors)])
         t.pensize(x / 50)
         t.forward(x)
         t.left(59)
+
 
 draw()
 time.sleep(5)
